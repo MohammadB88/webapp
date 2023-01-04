65c2e63c2510c2b013b60850c887ebe67e178d97
MohammadB88 <bahmani.m.n@gmail.com>
2023-01-04 13:36:34 +0100

    add addministrative access to the database

--- mosesreigns_fullstack/server/index.js
+++ mosesreigns_fullstack/server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Add mysql database connection
 const db = mysql.createPool({
-  host: 'mysql_db', // the host name MYSQL_DATABASE: node_mysql
+  host: 'mysql-db-svc', // the host name MYSQL_DATABASE: node_mysql
   user: 'MYSQL_USER', // database user MYSQL_USER: MYSQL_USER
   password: 'MYSQL_PASSWORD', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
   database: 'books' // database name MYSQL_HOST_IP: mysql_db
