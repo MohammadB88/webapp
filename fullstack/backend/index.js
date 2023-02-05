cc762b42113dbb8dd1b856d2e3343a9bb7b08bfa
MohammadB88 <bahmani.m.n@gmail.com>
2023-02-05 13:52:34 +0100

    update the backend and frontend using ChatGPT

--- fullstack/backend/index.js
+++ fullstack/backend/index.js
@@ -1,92 +1,167 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mysql = require('mysql2');
-const cors = require('cors');
+
 const app = express();
+app.use(bodyParser.json());
 
-// Add mysql database connection
-const db = mysql.createPool({
-  host: process.env.MYSQL_HOST_IP, //'mysql-db-svc', // the host name MYSQL_DATABASE: node_mysql
-  user: process.env.MYSQL_USER, //'MYSQL_USER', // database user MYSQL_USER: MYSQL_USER
-  password: process.env.MYSQL_PASSWORD, //'MYSQL_PASSWORD', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
-  database: process.env.MYSQL_DATABASE //'books' // database name MYSQL_HOST_IP: mysql_db
-})
-
-// Enable cors security headers
-app.use(cors())
-
-// add an express method to parse the POST method
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-
-// home page
-app.get('/', (req, res) => {
-  res.send('Hi There')
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST_IP,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
-// get all of the books in the database
-app.get('/get', (req, res) => {
-  const SelectQuery = "SELECT * FROM  books_reviews";
-  db.query(SelectQuery, (err, result) => {
-     if(err)
-  {
-     console.log(err)
-  }else{
-     console.log('GOT')
-  }
-     res.send(result)
-  })
-})
+app.get('/reviews', (req, res) => {
+  const sql = `SELECT * FROM reviews`;
+  connection.query(sql, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Failed to retrieve reviews' });
+      return;
+    }
+    res.send(results);
+  });
+});
 
-// add a book to the database
-app.post("/insert", (req, res) => {
-  const bookName = req.body.setBookName;
-  const bookReview = req.body.setReview;
-  const InsertQuery = "INSERT INTO books_reviews (book_name, book_review) VALUES (?, ?)";
-  db.query(InsertQuery, [bookName, bookReview], (err, result) => {
-     if(err)
-  {
-     console.log(err)
-  }else{
-     console.log('INSERTED')
-  }
-	// console.log(result)
-  })
-})
+app.post('/reviews', (req, res) => {
+  const { title, author, rating, review } = req.body;
+  const sql = `INSERT INTO reviews (title, author, rating, review) VALUES (?, ?, ?, ?)`;
+  const params = [title, author, rating, review];
+  connection.query(sql, params, (error, result) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Failed to submit review' });
+      return;
+    }
+    res.send({ message: 'Review submitted successfully!' });
+  });
+});
 
-// delete a book from the database
-app.delete("/delete/:bookId", (req, res) => {
-  const bookId = req.params.bookId;
-  const DeleteQuery = "DELETE FROM books_reviews WHERE id = ?";
-  db.query(DeleteQuery, bookId, (err, result) => {
-     if(err)
-  {
-     console.log(err)
-  }else{
-     console.log('DELETED')
-  }
-  })
-})
+app.put('/reviews/:id', (req, res) => {
+  const { title, author, rating, review } = req.body;
+  const { id } = req.params;
+  const sql = `UPDATE reviews SET title = ?, author = ?, rating = ?, review = ? WHERE id = ?`;
+  const params = [title, author, rating, review, id];
+  connection.query(sql, params, (error, result) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Failed to update review' });
+      return;
+    }
+    res.send({ message: 'Review updated successfully!' });
+  });
+});
 
-// update a book review
-app.put("/update/:bookId", (req, res) => {
-  const bookReview = req.body.reviewUpdate;
-  const bookId = req.params.bookId;
-  const UpdateQuery = "UPDATE books_reviews SET book_review = ? WHERE id = ?";
-  db.query(UpdateQuery, [bookReview, bookId], (err, result) => {
-     if(err)
-  {
-     console.log(err)
-  }else{
-     console.log('UPDATED')
-  }
-  })
-})
+app.delete('/reviews/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = `DELETE FROM reviews WHERE id = ?`;
+  const params = [id];
+  connection.query(sql, params, (error, result) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Failed to delete review' });
+      return;
+    }
+    res.send({ message: 'Review deleted successfully!' });
+  });
+});
 
-app.listen('3001', (err) => { 
-     if(err)
-  {
-     console.log(err)
-  }else{
-     console.log('ON PORT 3001')
-  }
-})
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Backend app listening on port ${port}`);
+});
+
+
+// const express = require('express');
+// const mysql = require('mysql2');
+// const cors = require('cors');
+// const app = express();
+
+// // Add mysql database connection
+// const db = mysql.createPool({
+//   host: process.env.MYSQL_HOST_IP, //'mysql-db-svc', // the host name MYSQL_DATABASE: node_mysql
+//   user: process.env.MYSQL_USER, //'MYSQL_USER', // database user MYSQL_USER: MYSQL_USER
+//   password: process.env.MYSQL_PASSWORD, //'MYSQL_PASSWORD', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
+//   database: process.env.MYSQL_DATABASE //'books' // database name MYSQL_HOST_IP: mysql_db
+// })
+
+// // Enable cors security headers
+// app.use(cors())
+
+// // add an express method to parse the POST method
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }));
+
+// // home page
+// app.get('/', (req, res) => {
+//   res.send('Hi There')
+// });
+
+// // get all of the books in the database
+// app.get('/get', (req, res) => {
+//   const SelectQuery = "SELECT * FROM  books_reviews";
+//   db.query(SelectQuery, (err, result) => {
+//      if(err)
+//   {
+//      console.log(err)
+//   }else{
+//      console.log('GOT')
+//   }
+//      res.send(result)
+//   })
+// })
+
+// // add a book to the database
+// app.post("/insert", (req, res) => {
+//   const bookName = req.body.setBookName;
+//   const bookReview = req.body.setReview;
+//   const InsertQuery = "INSERT INTO books_reviews (book_name, book_review) VALUES (?, ?)";
+//   db.query(InsertQuery, [bookName, bookReview], (err, result) => {
+//      if(err)
+//   {
+//      console.log(err)
+//   }else{
+//      console.log('INSERTED')
+//   }
+// 	// console.log(result)
+//   })
+// })
+
+// // delete a book from the database
+// app.delete("/delete/:bookId", (req, res) => {
+//   const bookId = req.params.bookId;
+//   const DeleteQuery = "DELETE FROM books_reviews WHERE id = ?";
+//   db.query(DeleteQuery, bookId, (err, result) => {
+//      if(err)
+//   {
+//      console.log(err)
+//   }else{
+//      console.log('DELETED')
+//   }
+//   })
+// })
+
+// // update a book review
+// app.put("/update/:bookId", (req, res) => {
+//   const bookReview = req.body.reviewUpdate;
+//   const bookId = req.params.bookId;
+//   const UpdateQuery = "UPDATE books_reviews SET book_review = ? WHERE id = ?";
+//   db.query(UpdateQuery, [bookReview, bookId], (err, result) => {
+//      if(err)
+//   {
+//      console.log(err)
+//   }else{
+//      console.log('UPDATED')
+//   }
+//   })
+// })
+
+// app.listen('3001', (err) => { 
+//      if(err)
+//   {
+//      console.log(err)
+//   }else{
+//      console.log('ON PORT 3001')
+//   }
+// })
--- fullstack/frontend/src/App.js
+++ fullstack/frontend/src/App.js
@@ -1,104 +1,241 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-restricted-globals */
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
-import { Button, Container, Card, Row } from 'react-bootstrap';
 
-// Import the background image
-//import background from './background.png';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-      this.state = {
-        setBookName: '',
-        setReview: '',
-        fetchData: [],
-        reviewUpdate: ''
-      }
-  }
+function App() {
+  const [reviews, setReviews] = useState([]);
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState("");
+  const [review, setReview] = useState("");
+  const [editId, setEditId] = useState(null);
 
-  handleChange = (event) => {
-    let nam = event.target.name;
-    let val = event.target.value
-    this.setState({
-      [nam]: val
-    })
-  }
+  useEffect(() => {
+    getReviews();
+  }, []);
 
-  handleChange2 = (event) => {
-    this.setState({
-      reviewUpdate: event.target.value
-    })
-  }
-
-  componentDidMount() {
-    axios.get("/api/get")
-      .then((response) => {
-        this.setState({
-          fetchData: response.data
-        })
-      })
-  }
-
-  submit = () => {
-    axios.post('/api/insert', this.state)
-      .then(() => { alert('success post') })
-    console.log(this.state)
-    document.location.reload();
-  }
-
-  delete = (id) => {
-    if (confirm("Do you want to delete? ")) {
-      axios.delete(`/api/delete/${id}`)
-      document.location.reload()
+  const getReviews = async () => {
+    try {
+      const res = await axios.get("/api/reviews");
+      setReviews(res.data);
+    } catch (error) {
+      console.error(error);
     }
-  }
+  };
 
-  edit = (id) => {
-    axios.put(`/api/update/${id}`, this.state)
-    document.location.reload();
-  }
+  const addReview = async e => {
+    e.preventDefault();
+    try {
+      if (editId === null) {
+        const res = await axios.post("/api/reviews", {
+          author,
+          title,
+          rating,
+          review
+        });
+        setReviews([...reviews, res.data]);
+      } else {
+        const res = await axios.put(`/api/reviews/${editId}`, {
+          author,
+          title,
+          rating,
+          review
+        });
+        const updatedReviews = reviews.map(review =>
+          review.id === editId ? res.data : review
+        );
+        setReviews(updatedReviews);
+        setEditId(null);
+      }
+      setAuthor("");
+      setTitle("");
+      setRating("");
+      setReview("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-  render() {
+  const deleteReview = async id => {
+    try {
+      await axios.delete(`/api/reviews/${id}`);
+      const updatedReviews = reviews.filter(review => review.id !== id);
+      setReviews(updatedReviews);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    let card = this.state.fetchData.map((val, key) => {
-      return (
-        <React.Fragment>
-          <Card style={{ width: '18rem' }} className='m-2'>
-            <Card.Body>
-              <Card.Title>{val.book_name}</Card.Title>
-              <Card.Text>
-                {val.book_review}
-              </Card.Text>
-              <input name='reviewUpdate' onChange={this.handleChange2} placeholder='Update Review' ></input>
-              <Button className='m-2' onClick={() => { this.edit(val.id) }}>Update</Button>
-              <Button onClick={() => { this.delete(val.id) }}>Delete</Button>
-            </Card.Body>
-          </Card>
-        </React.Fragment>
-      )
-    })
+  const editReview = review => {
+    setAuthor(review.author);
+    setTitle(review.title);
+    setRating(review.rating);
+    setReview(review.review);
+    setEditId(review.id);
+  };
 
-    return (
-     // <div className='App' style={{backgroundImage: `url(${background})`}}>
-      <div className='body'>
-        <h1>Dockerized Fullstack React Application</h1>
-        <div className='form'>
-          <input name='setBookName' placeholder='Enter Book Name' onChange={this.handleChange} />
-          <input name='setReview' placeholder='Enter Review' onChange={this.handleChange} />
-        </div>
-
-        <Button className='my-2' variant="primary" onClick={this.submit}>Submit</Button> <br /><br/>
-
-        <Container>
-          <Row>
-            {card}
-          </Row>
-        </Container>
-      </div>
-    );
-  } 
+  return (
+    <div className="container">
+      <h1>Book Review App</h1>
+      <form onSubmit={addReview}>
+        <label>Author:</label>
+        <input
+          type="text"
+          value={author}
+          onChange={e => setAuthor(e.target.value)}
+        />
+        <label>Title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <label>Rating:</label>
+        <input
+          type="number"
+          value={rating}
+          onChange={e => setRating(e.target.value)}
+        />
+        <label>Review:</label>
+        <textarea
+          value={review}
+          onChange={e => setReview(e.target.value)}
+        />
+        <button type="submit">{editId ? "Update" : "Add"} Review</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Author</th>
+            <th>Title</th>
+            <th>Rating</th>
+            <th>Review</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reviews.map(review => (
+            <tr key={review.id}>
+              <td>{review.author}</td>
+              <td>{review.title}</td>
+              <td>{review.rating}</td>
+              <td>{review.review}</td>
+              <td>
+                <button onClick={() => editReview(review)}>Edit</button>
+                <button onClick={() => deleteReview(review.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
+
 export default App;
+
+
+// /* eslint-disable no-unused-expressions */
+// /* eslint-disable no-restricted-globals */
+// import React, { Component } from 'react';
+// import './App.css';
+// import axios from 'axios';
+// import { Button, Container, Card, Row } from 'react-bootstrap';
+
+// // Import the background image
+// //import background from './background.png';
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//       this.state = {
+//         setBookName: '',
+//         setReview: '',
+//         fetchData: [],
+//         reviewUpdate: ''
+//       }
+//   }
+
+//   handleChange = (event) => {
+//     let nam = event.target.name;
+//     let val = event.target.value
+//     this.setState({
+//       [nam]: val
+//     })
+//   }
+
+//   handleChange2 = (event) => {
+//     this.setState({
+//       reviewUpdate: event.target.value
+//     })
+//   }
+
+//   componentDidMount() {
+//     axios.get("/api/get")
+//       .then((response) => {
+//         this.setState({
+//           fetchData: response.data
+//         })
+//       })
+//   }
+
+//   submit = () => {
+//     axios.post('/api/insert', this.state)
+//       .then(() => { alert('success post') })
+//     console.log(this.state)
+//     document.location.reload();
+//   }
+
+//   delete = (id) => {
+//     if (confirm("Do you want to delete? ")) {
+//       axios.delete(`/api/delete/${id}`)
+//       document.location.reload()
+//     }
+//   }
+
+//   edit = (id) => {
+//     axios.put(`/api/update/${id}`, this.state)
+//     document.location.reload();
+//   }
+
+//   render() {
+
+//     let card = this.state.fetchData.map((val, key) => {
+//       return (
+//         <React.Fragment>
+//           <Card style={{ width: '18rem' }} className='m-2'>
+//             <Card.Body>
+//               <Card.Title>{val.book_name}</Card.Title>
+//               <Card.Text>
+//                 {val.book_review}
+//               </Card.Text>
+//               <input name='reviewUpdate' onChange={this.handleChange2} placeholder='Update Review' ></input>
+//               <Button className='m-2' onClick={() => { this.edit(val.id) }}>Update</Button>
+//               <Button onClick={() => { this.delete(val.id) }}>Delete</Button>
+//             </Card.Body>
+//           </Card>
+//         </React.Fragment>
+//       )
+//     })
+
+//     return (
+//      // <div className='App' style={{backgroundImage: `url(${background})`}}>
+//       <div className='body'>
+//         <h1>Dockerized Fullstack React Application</h1>
+//         <div className='form'>
+//           <input name='setBookName' placeholder='Enter Book Name' onChange={this.handleChange} />
+//           <input name='setReview' placeholder='Enter Review' onChange={this.handleChange} />
+//         </div>
+
+//         <Button className='my-2' variant="primary" onClick={this.submit}>Submit</Button> <br /><br/>
+
+//         <Container>
+//           <Row>
+//             {card}
+//           </Row>
+//         </Container>
+//       </div>
+//     );
+//   } 
+// }
+// export default App;
