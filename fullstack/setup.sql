552eb7257f23d3c1d76566688b9984383bfe7d69
MohammadB88 <bahmani.m.n@gmail.com>
2023-02-05 13:53:11 +0100

    update the manifests and database initial file accordingly

--- fullstack/setup.sql
+++ fullstack/setup.sql
@@ -1,6 +1,19 @@
-CREATE TABLE IF NOT EXISTS `books_reviews` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `book_name` varchar(50) NOT NULL,
-  `book_review` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+CREATE DATABASE book_reviews;
+USE book_reviews;
+
+CREATE TABLE reviews (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  rating INT NOT NULL,
+  review TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- CREATE TABLE IF NOT EXISTS `books_reviews` (
+--   `id` int(11) NOT NULL AUTO_INCREMENT,
+--   `book_name` varchar(50) NOT NULL,
+--   `book_review` varchar(50) NOT NULL,
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
