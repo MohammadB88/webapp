6f3d8ce8c88601212abef268c74cb9633dbf58e9
MohammadB88 <bahmani.m.n@gmail.com>
2023-02-06 08:25:25 +0100

    clean up the files

--- fullstack/frontend/src/index.js
+++ fullstack/frontend/src/index.js
@@ -1,20 +1,10 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/main.css';
+// import './styles/main.css';
 import App from './App';
 
 ReactDOM.render(
   <App />, 
   document.getElementById('root')
 );
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
